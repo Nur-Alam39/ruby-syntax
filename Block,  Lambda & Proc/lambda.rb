f81efe85bb28bbc2a619bb88bae72fac1be9b1d2
sajoncu <saifulislamsajon>
2020-10-13 09:52:35 +0600

    source code

--- Block,  Lambda & Proc/lambda.rb
+++ Block,  Lambda & Proc/lambda.rb
@@ -3,3 +3,9 @@ say_something.call
 
 say_something = lambda {puts 'This is lambda as text'}
 say_something.call
+
+my_lambda = -> {puts 'Lambda called'}
+my_lambda.call
+my_lambda.()
+my_lambda[]
+my_lambda.===
---
+++ Block,  Lambda & Proc/lambda_with_arg.rb
@@ -0,0 +1,2 @@
+times_two = ->(x){puts "#{x * 2}"}
+times_two.call(10)
